2e3bf6cea0662e6e425213578dd1d404b757e7e4
hyn <huangyingning@gmail.com>
2017-07-07 22:54:09 +0800

    optimize K2 firmware; support Chinese SSID.

--- trunk/user/shared/defaults.h
+++ trunk/user/shared/defaults.h
@@ -35,7 +35,7 @@
 #define DEF_LAN_MASK		"255.255.255.0"
 
 #define DEF_WLAN_2G_CC		"CN"
-#define DEF_WLAN_5G_CC		"GB"
+#define DEF_WLAN_5G_CC		"CN"
 #define DEF_WLAN_2G_SSID	"ASUS"
 #define DEF_WLAN_5G_SSID	"ASUS_5G"
 #define DEF_WLAN_2G_GSSID	"ASUS_G"
